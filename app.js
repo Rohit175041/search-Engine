8e81371ada6951dc0da1f3df4c952ce1292b39a7
Rohit Kumar Singh <98216033+Rohit175041@users.noreply.github.com>
2023-01-25 21:44:30 +0530

    updated app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/searchQuery", (req, res) => {
     console.log(req.query);
     const category = req.query.q[0].toUpperCase() + req.query.q.slice(1);
     console.log(category);
-    let searching = `select *from product where category = '${category}'`
+    let searching = `select *from product where category = '${category}' limit 10`
     client.query(searching, (err, result) => {
         if (!err) {
             // console.log(result.rows);
@@ -28,7 +28,7 @@ app.get("/subCategory", (req, res) => {
     console.log(req.query);
     const subCategory = req.query.q[0].toUpperCase() + req.query.q.slice(1);
     console.log(subCategory);
-    let sortfilt = `select sub_category,product_id,product_name from product where sub_category ='${subCategory}' order by category desc`
+    let sortfilt = `select sub_category,product_id,product_name from product where sub_category ='${subCategory}' order by category desc limit 10`
     client.query(sortfilt, (err, result) => {
         if (!err) {
             // console.log(result.rows);
@@ -42,7 +42,7 @@ app.get("/filter", (req, res) => {
     console.log(req.query);
     const subCategory = req.query.q[0].toUpperCase() + req.query.q.slice(1);
     console.log(subCategory);
-    let filter = `select sub_category,product_id,product_name from product where sub_category ='${subCategory}'`
+    let filter = `select sub_category,product_id,product_name from product where sub_category ='${subCategory}' limit 10`
     client.query(filter, (err, result) => {
         if (!err) {
         }
@@ -53,7 +53,7 @@ app.get("/filter", (req, res) => {
 
 // all data
 app.get("/allProducts", (req, res) => {
-    let query = 'select *from product'
+    let query = 'select *from product limit 100'
     client.query(query, (err, result) => {
         if (!err) {
             // console.log(result.rows);
@@ -74,3 +74,33 @@ app.get("/allProducts", (req, res) => {
 // })
 
 app.listen("5000", () => console.log("server listening on port 5000"))
+
+
+
+
+
+
+// Backend:
+// Indexing and storing product information in a database.
+// Search functionality to query the database and retrieve relevant products based on user's search keywords.
+// Sorting and filtering
+// Integration with external APIs for retrieving product information.
+
+
+//Sample Output: 
+
+// 20230125193158
+// http://localhost:5000/allProducts
+// [
+//     {
+//       "product_id": "﻿FUR-BO-10001798",
+//       "category": "Furniture",
+//       "sub_category": "Bookcases",
+//       "product_name": "Bush Somerset Collection Bookcase"
+//     },
+//     {
+//       "product_id": "FUR-CH-10000454",
+//       "category": "Furniture",
+//       "sub_category": "Chairs",
+//       "product_name": "Hon Deluxe Fabric Upholstered Stacking Chairs  Rounded Back"
+//     },
