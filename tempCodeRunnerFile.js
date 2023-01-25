c52bbaba5ab05d2491e5f1f8d1fe3e14a55a93e1
Rohit Kumar Singh <98216033+Rohit175041@users.noreply.github.com>
2023-01-25 19:29:26 +0530

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-
-// client.query(`INSERT INTO user_data (user_id,name,email_id,password)
-// VALUES (2, 'Rohit Kumar Singh','rohit175041@gmail.com','rohitpassword')`,(err,result)=>{
-//     if(!err){
-//         console.log(result.rows);
-//     }
-//     //database connection end
-//     client.end()
-// })
